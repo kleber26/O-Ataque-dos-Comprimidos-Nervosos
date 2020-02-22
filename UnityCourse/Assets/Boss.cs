da31649653a84870fe86b226f7dc200c77b2bbf0
Kleber Lauton Guimaraes <kleber.guimaraes@tfgco.com>
2020-02-21 22:08:59 -0300

    [update] added new things

---
+++ UnityCourse/Assets/Boss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Boss : MonoBehaviour {
+
+//    public GameObject boss;
+//    private Transform player;
+//    private bool bossActivated;
+//    
+//    void Start() {
+//        player = GameObject.FindGameObjectWithTag("Player").transform;
+//    }
+//    
+//    private void OnTriggerEnter(Collider other) {
+//        if (!bossActivated) {
+//            boss.active = true;
+//            boss.GetComponent<NavMeshAgent>().SetDestination(player.position);
+//            bossActivated = true;
+//        }
+//    }
+    
+}
